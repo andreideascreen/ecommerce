409c19d3b37a807f367de6d21c017fc99b6da09b
andreideascreen <alpmdp@gmail.com>
2018-12-06 23:29:53 -0200

    Categoria produtos

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Celulares</a></li><li><a href="/categories/4">Notebook</a></li><li><a href="/categories/6">Televisores</a></li>
