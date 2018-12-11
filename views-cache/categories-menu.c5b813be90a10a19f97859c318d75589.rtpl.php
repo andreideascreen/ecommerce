cce84312d07e2a826ea7e22b4251b56f453384ee
andreideascreen <alpmdp@gmail.com>
2018-12-11 10:21:30 -0200

    Pagination e categorias

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Celulares</a></li><li><a href="/categories/4">Notebook</a></li><li><a href="/categories/6">Televisores</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Android</a></li><li><a href="/categories/2">Celulares</a></li><li><a href="/categories/1">Google</a></li><li><a href="/categories/3">Notebook</a></li>
